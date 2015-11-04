2bd14d0d76403a472f489bb5379bc5a55bff49b1
Evan Allan <evanallan1026@gmail.com>
2015-11-04 10:27:31 -0500

    Cleanup of unneeded code

--- src/components/ScrollView.cpp
+++ src/components/ScrollView.cpp
@@ -33,57 +33,5 @@ void ScrollView::render() {
         }
     }
 
-    /*********************************************************
-     *    What follows was attempted code for scroll bars    *
-     * I gave up on them because they looked dumb and didn't *
-     *             really work correctly anyway.             *
-     *   I'm leaving this in for one commit just in case I   *
-     *              want to revisit this later.              *
-     ********************************************************/
-
-    /*
-    int hbarWidth  = std::max(1, (int)(((float) d.width / (float) in_d.width) * (d.width - 2)));
-    int vbarHeight = std::max(1, (int)(((float) d.height / (float) in_d.height) * (d.height - 2)));
-
-    int hbarStart = (int)(((float) in_d.x / (float) in_d.width) * (d.width - 2)) + 1;
-    int vbarStart = (int)(((float) in_d.y / (float) in_d.height) * (d.height - 2)) + 1;
-
-    start_color();
-    init_pair(1, COLOR_BLACK, COLOR_WHITE);
-    init_pair(2, COLOR_BLACK, COLOR_RED);
-
-    for (int i = 1; i < d.width - 1; ++i) {
-        if (i >= hbarStart && i < hbarStart + hbarWidth) {
-            wmove(window, d.height - 1, i);
-            wattron(window, COLOR_PAIR(2));
-            waddch(window, ' ');
-            wattroff(window, COLOR_PAIR(2));
-        } else {
-            wattron(window, COLOR_PAIR(1));
-            mvwaddch(window, d.height - 1, i, ' ');
-            wattroff(window, COLOR_PAIR(1));
-        }
-    }
-
-    for (int i = vbarStart; i < vbarStart + vbarHeight; ++i) {
-        mvwaddch(window, i, d.width - 1, ACS_CKBOARD);
-    }
-
-    std::stringstream ss;
-    ss << "hbarStart: " << hbarStart;
-    const std::string hbarString = ss.str();
-    ss.str("");
-    ss << "vbarStart: " << vbarStart;
-    const std::string vbarString = ss.str();
-
-    erase();
-
-    move(50,50);
-    printw(hbarString.c_str());
-    move(51,50);
-    printw(vbarString.c_str());
-
-    refresh();
-    */
     wrefresh(window);
 }
